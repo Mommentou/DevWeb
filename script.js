856364822fbd39492951420637873523116d286e
Kevin <esterle.kevin@gmail.com>
2024-02-08 11:14:16 +0100

    Javascript

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
-document.addEventListener('DOMContentLoaded', function(){alert('La page est complétement chargée !'); });
+var button = document.getElementById('theme_btn');
+
+var body = document.getElementsByTagName('body')[0];
+
+var light_theme = 'light';
+
+button.addEventListener('click', function() {
+    if (body.classList.contains(light_theme)) {
+        body.classlist.remove(light_theme);
+    }
+    else {
+        body.classList.add(light_theme);
+    }
+} );
